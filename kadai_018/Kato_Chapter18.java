ebdd5e290a0440113a71fe5bf06a78846d487c45
miyagawa-1869 <answer.taba9@gmail.com>
2024-08-19 21:31:21 +0900

    Add files via upload
    課題の修正

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -3,14 +3,12 @@ package kadai_018;
 //親クラス
 public abstract class Kato_Chapter18 {
 	//	フィールド
-	public String familyName;
+	public String familyName="加藤";
 	public String givenName;
-	public String address;
+	public String address="東京都中野区〇×";
 
 	//共通の紹介メソッド
 	public void commonIntroduce() {
-		familyName = "加藤";
-		address = "東京都中野区〇×";
 		System.out.println("名前は" + familyName + givenName + "です");
 		System.out.println("住所は" + address + "です");
 	}
